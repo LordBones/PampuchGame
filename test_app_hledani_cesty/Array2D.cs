cdb67a6e1a1fa8381c1e3cf0fe4e4e39e0af374b
Jakub balarin <jakub.balarin@webtica.com>
2013-11-14 23:35:13 +0100

    zapoceti noveho vypoctu hledani nejkratsi cesty

---
+++ test_app_hledani_cesty/Array2D.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenArt.Core.Classes
+{
+    public class Array2D
+    {
+        public byte [] Data;
+        protected int _width;
+        protected int _height;
+
+
+        public Array2D(int width, int height) :
+            this(width, height, 1)
+        {
+        }
+
+        protected Array2D(int width, int height, int stride)
+        {
+            this.Data = new byte[width * height * stride];
+            this._width = width;
+            this._height = height;
+        }
+
+        public int Width
+        {
+            get { return _width; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+        }
+
+        public int Length
+        {
+            get { return this.Data.Length; }
+        }
+
+    }
+
+    public class Array2D<T>
+    {
+        public T [] Data;
+        protected int _width;
+        protected int _height;
+
+
+        public Array2D(int width, int height) :
+            this(width, height, 1)
+        {
+        }
+
+        protected Array2D(int width, int height, int stride)
+        {
+            this.Data = new T[width * height * stride];
+            this._width = width;
+            this._height = height;
+        }
+
+        public void FillByValue(T value)
+        {
+            for (int i = 0; i < Data.Length; i++)
+                Data[i] = value;
+        }
+
+        public int Width
+        {
+            get { return _width; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+        }
+
+        public int Length
+        {
+            get { return this.Data.Length; }
+        }
+
+        public T this[int x, int y]
+        {
+            get { int index = y * _width + x; return Data[index]; }
+            set { int index = y * _width + x; Data[index] =value; }
+        }
+
+    }
+}
---
+++ test_app_hledani_cesty/WeightSearchShortPath.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenArt.Core.Classes;
+
+namespace test_app_hledani_cesty
+{
+    public class WeightSearchShortPath
+    {
+
+
+        public enum MazeTypeObject { Empty = 0, Wall = 1 }
+
+        /// <summary>
+        /// value from 0 - 255 
+        /// higher value means, path cross this field is longer.
+        /// </summary>
+        private Array2D<byte> _areaWeights;
+        private Array2D<byte> _area;
+        private Array2D<int> _areaSumWeights;
+
+
+
+        public WeightSearchShortPath(int width, int height, Array2D<byte> area)
+        {
+            if (width != area.Width || height != area.Height) throw new Exception();
+
+            _areaWeights = new Array2D<byte>(width,height);
+            _area = new Array2D<byte>(width, height);
+            _areaSumWeights = new Array2D<int>(width, height);
+
+            for (int i = 0; i < _area.Length; i++)
+            {
+                byte tmp = area.Data[i];
+                if (tmp == (int)MazeTypeObject.Empty) _area.Data[i] = (int)MazeTypeObject.Empty;
+                else if (tmp == (int)MazeTypeObject.Wall) _area.Data[i] = (int)MazeTypeObject.Wall;
+                else _area.Data[i] = (int)MazeTypeObject.Empty;
+            }
+        }
+
+
+        public void FindShortestPath(Point start, Point end)
+        {
+            ComputeSumWeight(start, end);
+        }
+
+        private void ComputeSumWeight(Point start, Point end)
+        {
+            _areaSumWeights.FillByValue(-1);
+            _areaSumWeights[start.X, start.Y] = 0;
+
+            Queue<Point> queueForSearch = new Queue<Point>();
+            queueForSearch.Enqueue(start);
+        }
+    }
+}
